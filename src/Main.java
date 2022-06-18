0630df9ce48c10c7ee00422d08cc97825a11a3fb
kaninaoksana <oksana_1710@mail.ru>
2022-06-19 00:04:09 +0300

    задание2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 5000;
+
+        int add = 1200;
+        int bonus = 0;
+        if (add > 1000) {
+            bonus = add / 100;
+        }
+        int end=start+add+bonus;
+
+        System.out.println("итоговый бонус:" + bonus );
+        System.out.println("Итоговый счет:" + end );
+    }
+}
